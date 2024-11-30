de6d19cdfa69ef50d391f8c6c3648ab129c7b8f2
root <root@plug-ha.local>
2024-11-30 07:53:02 +0000

    another code cleaning routine

--- custom_components/mq_praying_times/config_flow.py
+++ custom_components/mq_praying_times/config_flow.py
@@ -21,7 +21,7 @@ class MQPrayingTimesConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
         vol.Required('ikhtiyat_ashar', default=2): vol.Coerce(int),
         vol.Required('ikhtiyat_maghrib', default=2): vol.Coerce(int),
         vol.Required('ikhtiyat_isya', default=2): vol.Coerce(int),
-        vol.Required('ikhtiyat_syuruq', default=2): vol.Coerce(int),
+        vol.Required('ikhtiyat_syuruq', default=-2): vol.Coerce(int),
         vol.Required('ikhtiyat_dhuha', default=2): vol.Coerce(int),
     })
 
