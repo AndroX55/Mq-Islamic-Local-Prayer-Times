33611b38de681afc9daf7ee0cb4421858e857f54
AndroX55 <62584931+AndroX55@users.noreply.github.com>
2025-06-14 08:47:49 +0700

    Update __init__.py

--- custom_components/mq_praying_times/__init__.py
+++ custom_components/mq_praying_times/__init__.py
@@ -8,7 +8,7 @@ async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
     
     # setting up a sensor platform
     hass.async_create_task(
-        hass.config_entries.async_forward_entry_setups(entry, "sensor")
+        hass.config_entries.async_forward_entry_setups(entry, ["sensor"])
     )
 
     return True
